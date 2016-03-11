6b39ef769fd5a03eac34099b8e0f3aceff15bcd7
Thr1ve <fict1on.buhler@gmail.com>
2016-03-11 15:26:02 -0500

    (feat) initial socket.io test example

--- app/index.js
+++ app/index.js
@@ -2,8 +2,42 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
 
+var io = require('socket.io-client');
+var socket = io('http://localhost:8090')
+
+socket.on('connection', function() {
+  console.log('connected');
+});
+
+socket.on('test', function(data) {
+  console.log(data);
+})
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      n: 0
+    }
+  },
+
+  componentDidMount: function() {
+    var self = this;
+    socket.on('n', function(data) {
+      console.log(data);
+      self.setState({n: data})
+    })
+  },
+
+  render: function() {
+    return (
+      <div> the number is: {this.state.n} </div>
+    )
+  }
+})
+
+
 
 ReactDOM.render(
-  routes,
+  <App />,
   document.getElementById('app')
 );
