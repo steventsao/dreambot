4cd887290bd415068cfb3b7abd58207647a30ffc
Colton Savage <csavage1994@gmail.com>
2016-03-11 18:10:44 -0600

    (feat) Add sentiment analysis for every post

---
+++ server/testDB.js
@@ -0,0 +1,14 @@
+
+var r = require('rethinkdb');
+r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
+  if(err) throw err;
+  r.db('test').tableCreate('tv_shows').run(conn, function(err, res) {
+    if(err) throw err;
+    console.log(res);
+    r.table('tv_shows').insert({ name: 'Star Trek TNG' }).run(conn, function(err, res)
+    {
+      if(err) throw err;
+      console.log(res);
+    });
+  });
+});
