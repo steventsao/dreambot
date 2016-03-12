d4c8052351fe967bef83a27ca523afad644ea3f3
Colton Savage <csavage1994@gmail.com>
2016-03-11 21:56:14 -0600

    (feat) Add parsed messages to parsedMessages table

--- server/botModel.js
+++ server/botModel.js
@@ -5,22 +5,52 @@ r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
   if(err) throw err;
   globalConn = conn;
 
+  //creates tables TODO: make run only once
+
   // creates db...commented out because the db has been created
-  r.db('test').tableCreate('rawMessages').run(conn, function(err, res) {
-    if(err) throw err;
-    console.log(res);
-  });
-  r.db('test').tableCreate('parsedMessages').run(conn, function(err, res) {
-    if(err) throw err;
-    console.log(res);
-  });
+   // r.db('test').tableCreate('rawMessages').run(conn, function(err, res) {
+  //   if(err) throw err;
+  //   console.log(res);
+  // });
+
+  // r.db('test').tableCreate('parsedMessages').run(conn, function(err, res) {
+  //   if(err) throw err;
+  //   console.log(res);
+  // });
+
+
 });
 
 module.exports = function(msg){
   console.log('saving message');
-  r.table('rawMessages').insert(msg).run(globalConn, function(err, res)
+  //if there is existing data
+  // r.table('parsedMessages').hasFields('totalSentiment').run(globalConn, function(err, res){
+  //   //update data
+  // });
+
+  // //if there is no data in the table
+  // r.table('parsedMessages').filter(
+  //   r.row.hasFields('totalSentiment').not())
+  //   .run(globalConn, function(){
+  //       //insert starting data
+  //       r.table('parsedMessages').insert({'sentiment': sentiment(msg.text).comparative, 'totalSentiment': sentiment(msg.text).comparative, 'totalInputs': 1}).run(globalConn, function(err, res){
+  //         if(err){
+  //           throw err;
+  //         } else{
+  //           console.log(res);
+  //         }
+  //       });
+  // });
+
+  r.table('parsedMessages').insert(sentiment(msg.text)).run(globalConn, function(err, res){
+    if(err){
+      throw err;
+    } else{
+      console.log(res);
+    }
+  });
+
   console.log('saving message and anaylizing message');
-  console.log(sentiment(msg.text));
   r.table('dreambot').insert(msg).run(globalConn, function(err, res)
     {
       if(err) throw err;
