e15bf78c9a25a1666a27f7f5bda88ad88c8c6610
steventsao <steventsao713@gmail.com>
2016-03-12 07:54:51 -0800

    Implement WitAI to identify javascript related questions

--- server/botModel.js
+++ server/botModel.js
@@ -8,33 +8,27 @@ r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
   //creates tables TODO: make run only once
 
   // creates db...commented out because the db has been created
-   // r.db('test').tableCreate('rawMessages').run(conn, function(err, res) {
+  // r.db('test').tableCreate('rawMessages').run(conn, function(err, res) {
   //   if(err) throw err;
   //   console.log(res);
   // });
-
   // r.db('test').tableCreate('parsedMessages').run(conn, function(err, res) {
   //   if(err) throw err;
   //   console.log(res);
   // });
-
-
 });
 
 module.exports = function(msg){
   console.log('saving message');
-
-  r.table('parsedMessages').insert(sentiment(msg.text)).run(globalConn, function(err, res){
-    if(err){
-      throw err;
-    } else{
-      console.log(res);
-    }
-  });
-
+  r.table('rawMessages').insert(msg).run(globalConn, function(err, res){
   console.log('saving message and anaylizing message');
-  r.table('rawMessages').insert(msg).run(globalConn, function(err, res)
-    {
+  console.log(sentiment(msg.text));
+  require('./witai.js')(msg.text, function(res){
+    console.log('categorizing messages...')
+    console.log(res);
+  })
+});
+  r.table('dreambot').insert(msg).run(globalConn, function(err, res){
       if(err) throw err;
       console.log(res);
     });
