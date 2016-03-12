df22661b0e46326687ffcfb2a934d9df037db1c6
Colton Savage <csavage1994@gmail.com>
2016-03-11 21:57:45 -0600

    Update botModel.js

--- server/botModel.js
+++ server/botModel.js
@@ -23,24 +23,6 @@ r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
 
 module.exports = function(msg){
   console.log('saving message');
-  //if there is existing data
-  // r.table('parsedMessages').hasFields('totalSentiment').run(globalConn, function(err, res){
-  //   //update data
-  // });
-
-  // //if there is no data in the table
-  // r.table('parsedMessages').filter(
-  //   r.row.hasFields('totalSentiment').not())
-  //   .run(globalConn, function(){
-  //       //insert starting data
-  //       r.table('parsedMessages').insert({'sentiment': sentiment(msg.text).comparative, 'totalSentiment': sentiment(msg.text).comparative, 'totalInputs': 1}).run(globalConn, function(err, res){
-  //         if(err){
-  //           throw err;
-  //         } else{
-  //           console.log(res);
-  //         }
-  //       });
-  // });
 
   r.table('parsedMessages').insert(sentiment(msg.text)).run(globalConn, function(err, res){
     if(err){
