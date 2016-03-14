c50835972f319b71139a1c139e484e444e3b0f9a
Thr1ve <fict1on.buhler@gmail.com>
2016-03-14 01:35:38 -0400

    (feat) update webpack config for hot-reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,43 @@
+/* eslint-disable */
+
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
 });
 
+/*
+  Should we use this or just have a `webpack.config.js` and a `webpack.production.config.js`?
+*/
+var env = process.env.NODE_ENV;
+
+var WebpackEnvPlugin = new webpack.DefinePlugin({
+  'process.env.NODE_ENV': JSON.stringify(env)
+});
+
 module.exports = {
+  devtool: 'cheap-module-eval-source-map',
   entry: [
-  './app/index.js'
+    'webpack-hot-middleware/client?reload=true',
+    './app/index.js'
   ],
   output: {
     path: __dirname + '/dist',
-    filename: "index_bundle.js"
+    filename: 'index_bundle.js',
+    publicPath: '/'
   },
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [
+    HtmlWebpackPluginConfig,
+    WebpackEnvPlugin,
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ]
 };
