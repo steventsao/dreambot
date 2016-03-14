d533e4106e3e71c473280248477573e0020a0abf
Thr1ve <fict1on.buhler@gmail.com>
2016-03-14 01:33:27 -0400

    (feature) update server index for hot-reloading

--- server/index.js
+++ server/index.js
@@ -1,25 +1,26 @@
-var app = require('express')();
+var path = require('path');
+
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var r = require('rethinkdb');
+
+var webpack = require('webpack');
+var webpackMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+
+var config = require('../webpack.config.js');
 var connect = require('./utils/connect');
+var env = require('./utils/envDefaults');
 
 require('./bot/bot.js');
-
-
-server.listen(8090);
-
-// app.get('/', function (req, res) {
-//   res.sendfile(__dirname + '/index.html');
-// });
 
 // TODO: move this into it's own module? placement depends on file structure?
 // Returns a promise
 function getChangeFeed() {
   return connect()
-    .then(conn => {
-      return r.table('messages').changes().run(conn)
-    })
+    .then(conn => r.table('messages').changes().run(conn));
 }
 
 getChangeFeed()
@@ -27,16 +28,48 @@ getChangeFeed()
     cursor.each((err, change) => {
       console.log('change detected!', change);
       io.emit('test', change);
-    })
-  })
+    });
+  });
 
-io.on('connection', function(socket){
-  // var n = 0;
-  // console.log('a user connected');
-  // // socket.emit('test', {foo: 'bar'});
-  // setInterval(add, 1000);
-  // function add() {
-  //   n++;
-  //   socket.emit('n', n);
-  // }
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
+
+// Add routes to app here:
+// ex: app.use('/api', apiRoutes);
+
+// referenced https://github.com/christianalfoni/webpack-express-boilerplate
+if (env.isDev) {
+  var compiler = webpack(config);
+  var middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  });
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+  app.get('*', function response(req, res) {
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')));
+    res.end();
+  });
+} else {
+  app.use(express.static(__dirname + '../dist'));
+
+  // This redirects any GET requests that aren't for '/' or our above-mentioned
+  // routes to the home-page, letting the router on our SPA front-end handle it.
+  // This way, trying to refresh a specific page of the app won't
+  // end in a "cannot GET '/part/of/app'" error
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+}
+
+server.listen(env.port, () => console.log(`App Listening on port ${env.port}`));
