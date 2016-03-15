5b84caf8e9594d5a09078ab4664e01c0c0bbcaf8
danmolitor <danmolitor91@gmail.com>
2016-03-15 16:30:05 -0700

    (refactor) refactored to be presentation component, moved state logic into MessagesContainer

--- app/components/Messages.js
+++ app/components/Messages.js
@@ -2,30 +2,16 @@ import React from 'react';
 import io from 'socket.io-client';
 const socket = io('http://localhost:1337');
 
-const Messages = React.createClass({
-  getInitialState: function() {
-    return {
-      messages: []
-    }
-  },
-  componentDidMount: function() {
-    socket.on('test', (data) => {
-      let messages = this.state.messages.concat(data.new_val);
-      this.setState({messages: messages})
-      console.log(this.state);
-    });
-  },
-  render : function() {
-    return (
-      <div> Hello from DreamBot:
+const Messages = ({messages}) => {
+      return (
+        <div> Hello from DreamBot:
 
-      {this.state.messages.map(message => {
+      {messages.map(message => {
         return <span> <br/> {message.text} </span>
       })}
 
       </div>
     )
-  }
-})
+}
 
 export default Messages;
