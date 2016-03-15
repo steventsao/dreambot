2a1f0dfbf7ee6a0daa9ada9c564470925ddd61e5
danmolitor <danmolitor91@gmail.com>
2016-03-15 16:28:07 -0700

    (feat) added socket listener and dispatch to store to get messages

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,11 +1,17 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import rootReducer from '../reducers'
 import logger from 'redux-logger'
+import io from 'socket.io-client';
+const socket = io('http://localhost:1337');
+import { addMessage } from '../actions';
 
 // applying middleware
 
 export default function configureStore(initialState){
   const store = createStore(rootReducer);
+  socket.on('test', (data) => {
+    // console.log(data);
+    store.dispatch(addMessage(data.new_val));
+  });
   return store;
 }
-
