36f5229f8671f94db939dadafdce6f512ec19b1d
danmolitor <danmolitor91@gmail.com>
2016-03-16 13:45:21 -0700

    (feat) implemented chart with TS labels, individual message score, and average mood

--- app/components/Graph.js
+++ app/components/Graph.js
@@ -2,27 +2,34 @@ import React from 'react';
 import { Line as LineChart } from 'react-chartjs';
 import styles from '../styles';
 
-const Graph = ({messages}) => {
-      return (
-        <div style={styles.graph}> <br/> THE GRAPH OMG <br/>
-
-      <LineChart data={{
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
+const Graph = ({labels, data, dataAvg}) => {
+  const dataset = {
+      labels: labels,
+      datasets: [
         {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        }
-    ]
-}} width="600" height="250"/>
-
-      </div>
+          label: 'My First dataset',
+          fillColor: 'rgba(0,0,0,0.2)',
+          strokeColor: 'rgba(0,0,0,1)',
+          pointColor: 'rgba(0,0,0,1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(0,0,0,1)',
+          data: data
+        },
+        {
+          label: 'My First dataset',
+          fillColor: 'rgba(0,144,255,0.2)',
+          strokeColor: 'rgba(0,144,255,1)',
+          pointColor: 'rgba(0,144,255,1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(0,144,255,1)',
+          data: dataAvg
+        },
+      ]
+    };
+      return (
+      <LineChart style={styles.graph} data={dataset} width="600" height="250"/>
     )
 }
 
--- app/containers/GraphContainer.js
+++ app/containers/GraphContainer.js
@@ -5,14 +5,18 @@ import Graph from '../components/Graph';
 const GraphContainer = React.createClass({
   render: function() {
     return (
-      <Graph />
+      <Graph labels={this.props.labels} data={this.props.data} dataAvg={this.props.dataAvg} />
     )
   }
 })
 
 const mapStateToProps = (state) => {
-  return state;
-}
+  return {
+    labels: state.messages.map( (message) => new Date(message.ts * 1000).toLocaleDateString()),
+    data: state.messages.map( (message) => message.score ),
+    dataAvg: state.messages.map( (message) => message.comparative )
+  };
+};
 
 const GetGraph = connect(mapStateToProps)(GraphContainer);
 
