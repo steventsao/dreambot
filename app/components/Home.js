150ce1f06100072d2279556995ca72c9c79889f2
danmolitor <danmolitor91@gmail.com>
2016-03-15 19:42:26 -0700

    (feat) imported GraphContainer

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactRouter from 'react-router';
 import styles from '../styles';
 import MessagesContainer from '../containers/MessagesContainer';
+import GraphContainer from '../containers/GraphContainer';
 import NavbarContainer from '../containers/NavbarContainer';
 
 const Home = React.createClass({
   render: function () {
     return (
-      <div style={styles.title}>
-        <NavbarContainer/>
-        <h1>Dream Bot</h1>
+      <div>
+        <NavbarContainer />
+        <h1 style={styles.title}>Dream Bot</h1>
         <MessagesContainer />
+        <GraphContainer />
       </div>
     );
   }
