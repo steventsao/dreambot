ec30f6f6aae385717e844ef3012b8e313e844164
danmolitor <danmolitor91@gmail.com>
2016-03-16 14:07:19 -0700

    (refactor) deleted graphReducer

--- app/reducers/graphReducer.js
+++
@@ -1,8 +0,0 @@
-export default function graphReducer(state = {}, action) {
-  switch (action.type) {
-    case 'FETCH_MESSAGES':
-      return state.concat(action.msg);
-    default:
-      return state;
-  }
-}
