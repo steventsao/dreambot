6e0b87991e3b6bce47d29380e1bdb55b90f3b18f
Thr1ve <fict1on.buhler@gmail.com>
2016-03-17 14:57:12 -0400

    (feat) add byHour and byDayOfWeek queries

--- server/api/messages/messageRoutes.js
+++ server/api/messages/messageRoutes.js
@@ -4,6 +4,8 @@ var app = express.Router();
 
 var messageController = require('./messageController');
 
+app.use('/byHour', messageController.getMessagesByHour);
+app.use('/byDayOfWeek', messageController.getMessagesByDayOfWeek);
 app.use('/', messageController.getMessages);
 
 module.exports = app;
