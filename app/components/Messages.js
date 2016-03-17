b0659377dbfa40b89ac3aafb8af6817fc75b80f0
danmolitor <danmolitor91@gmail.com>
2016-03-16 17:15:12 -0700

    (refactor) messages sorted by timestamp

--- app/components/Messages.js
+++ app/components/Messages.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Messages = ({ messages }) => {
+  const comparator = (a, b) => { return b.ts - a.ts };
+  const sorted = messages.sort(comparator);
   return (
     <div>
-      {messages.map(message => {
+      {sorted.map(message => {
         return <div className="notification">{message.name}:{message.text} </div>;
       })}
     </div>
