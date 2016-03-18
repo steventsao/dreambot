1d0dd508b89b89cb2140694d2a75be8d57787421
steventsao <steventsao713@gmail.com>
2016-03-17 23:29:16 -0700

    Refactor containers

--- app/containers/NavbarContainer.js
+++ app/containers/NavbarContainer.js
@@ -2,47 +2,51 @@ import { connect } from 'react-redux';
 import React from 'react';
 import Navbar from '../components/Navbar';
 
-let NavbarContainer = React.createClass({
-  getAnalytics() {
-    let analytics = this.props.messages.reduce((acc, message) => {
-      acc.totalSentiment += message.score;
-      if (message.classification) {
-        acc.topics[message.classification] = acc.topics[message.classification]
-        ? acc.topics[message.classification] + 1
-        : 1;
-      }
-      if (message.name) {
-        acc.names[message.name] = acc.names[message]
-        ? [...acc.names[message], message]
-        : [];
-      }
+const getAnalytics = (messages) => {
+  let analytics = messages.reduce((acc, message) => {
+    acc.totalSentiment += message.score;
+    if (message.classification) {
+      acc.topics[message.classification] = acc.topics[message.classification]
+      ? acc.topics[message.classification] + 1
+      : 1;
+    }
+    if (message.name) {
+      acc.names[message.name] = acc.names[message]
+      ? [...acc.names[message], message]
+      : [];
+    }
 
-      return acc;
-    }, { names: {}, totalSentiment: 0, topics: {} });
+    return acc;
+  }, { names: {}, totalSentiment: 0, topics: {} });
 
-    let topThreeTopics = Object.keys(analytics.topics).sort((a, b) =>
-      analytics.topics[b] - analytics.topics[a]
-    ).slice(0, 3);
+  let topThreeTopics = Object.keys(analytics.topics).sort((a, b) =>
+    analytics.topics[b] - analytics.topics[a]
+  ).slice(0, 3);
 
-    let mostActiveUsers = Object.keys(analytics.names).sort((a, b) =>
-      analytics.names[b].length - analytics.names[a].length
-    );
+  let mostActiveUsers = Object.keys(analytics.names).sort((a, b) =>
+    analytics.names[b].length - analytics.names[a].length
+  );
 
-    analytics.topics = topThreeTopics;
-    analytics.mostActiveUsers = mostActiveUsers;
+  analytics.topics = topThreeTopics;
+  analytics.mostActiveUsers = mostActiveUsers;
 
-    return analytics;
-  },
-  render() {
-    const { messages, dispatch } = this.props;
-    const analytics = this.getAnalytics();
-    return (
-      <Navbar messages={messages} analytics={analytics} />
-      );
+  return analytics;
+};
+  // ,
+  // render() {
+  //   const { messages, dispatch } = this.props;
+  //   const analytics = this.getAnalytics();
+  //   return (
+  //     <Navbar messages={messages} analytics={analytics} />
+  //     );
+  // }
+
+let mapStateToProps = (state) => {
+  return {
+    messages: state.messages,
+    analytics: getAnalytics(state.messages)
   }
-});
-
-let mapStateToProps = (state) => state;
+};
 
 
-export default connect(mapStateToProps)(NavbarContainer);
+export default connect(mapStateToProps)(Navbar);
