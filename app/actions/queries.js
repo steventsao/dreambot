044627aa12fda423a48e9bbe2127a5ec5a53d248
Thr1ve <fict1on.buhler@gmail.com>
2016-03-20 15:27:38 -0400

    (feat) add getMessagesBy queries to front-end

--- app/actions/queries.js
+++ app/actions/queries.js
@@ -3,7 +3,8 @@ import { connection, r } from '../utils/rethink';
 export function getMessages() {
   return connection
     .then(conn =>
-      r.table('messages').run(conn)
+      r.table('messages')
+        .run(conn)
         .then(cursor => cursor.toArray())
     );
 }
@@ -11,9 +12,31 @@ export function getMessages() {
 export function getSearchResults(word) {
   return connection
     .then(conn =>
-      r.table('messages').filter(message =>
-        message('name').eq(word).or(message('tokens').contains(word)))
-      .run(conn)
-      .then(cursor => cursor.toArray())
+      r.table('messages')
+        .filter(message =>
+          message('name').eq(word).or(message('tokens').contains(word))
+        )
+        .run(conn)
+        .then(cursor => cursor.toArray())
+    );
+}
+
+export function getMessagesByHour() {
+  return connection
+    .then(conn =>
+      r.table('messages')
+        .group(r.row('ts').hours())
+        .run(conn)
+        .then(cursor => cursor.toArray())
+    );
+}
+
+export function getMessagesByDayOfWeek() {
+  return connection
+    .then(conn =>
+      r.table('messages')
+        .group(r.row('ts').dayOfWeek())
+        .run(conn)
+        .then(cursor => cursor.toArray())
     );
 }
