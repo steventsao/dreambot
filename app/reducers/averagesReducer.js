4fbbb2ed8e87ba93a6aa990a8df59b3095b1a4e4
Thr1ve <fict1on.buhler@gmail.com>
2016-03-21 15:55:59 -0400

    (chore) move default state to variable and use object spread operator

--- app/reducers/averagesReducer.js
+++ app/reducers/averagesReducer.js
@@ -1,25 +1,34 @@
 import { combineReducers } from 'redux';
+import moment from 'moment';
 
 import { REQUEST_AVERAGES, RECEIVE_AVERAGES } from '../actions';
 
-function byHour(state = { currentDate: '', isFetching: false, averages: {} }, action) {
+const initialState = {
+  displayedDate: {
+    year: moment().year(),
+    month: moment().month() + 1,
+    day: moment().date()
+  },
+  isFetching: false,
+  averages: {}
+};
+
+function byHour(state = initialState, action) {
   if (action.delimiter !== 'BY_HOUR') {
     return state;
   }
 
   switch (action.type) {
     case REQUEST_AVERAGES:
-      return Object.assign({}, state, { isFetching: true });
+      return { ...state, isFetching: true };
     case RECEIVE_AVERAGES:
       // wrap this case in a block? see http://eslint.org/docs/rules/no-case-declarations
       const { year, month, day } = action.date;
-      return Object.assign({}, state, {
-        currentDate: `${year}-${month}-${day}`,
+      return { ...state,
+        displayedDate: { ...state.displayedDate, year, month, day },
         isFetching: false,
-        averages: Object.assign({}, state.averages, {
-          [`${year}-${month}-${day}`]: action.averages
-        })
-      });
+        averages: { ...state.averages, [`${year}-${month}-${day}`]: action.averages }
+      };
     default:
       return state;
   }
