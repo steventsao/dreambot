07bca34b1d1faf2218f5e79271fd26c01d6716de
Thr1ve <fict1on.buhler@gmail.com>
2016-03-21 14:00:44 -0400

    (feat) add currentDate to state

--- app/reducers/averagesReducer.js
+++ app/reducers/averagesReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import { REQUEST_AVERAGES, RECEIVE_AVERAGES } from '../actions';
 
-function byHour(state = { isFetching: false, averages: {} }, action) {
+function byHour(state = { currentDate: '', isFetching: false, averages: {} }, action) {
   if (action.delimiter !== 'BY_HOUR') {
     return state;
   }
@@ -11,8 +11,10 @@ function byHour(state = { isFetching: false, averages: {} }, action) {
     case REQUEST_AVERAGES:
       return Object.assign({}, state, { isFetching: true });
     case RECEIVE_AVERAGES:
+      // wrap this case in a block? see http://eslint.org/docs/rules/no-case-declarations
       const { year, month, day } = action.date;
       return Object.assign({}, state, {
+        currentDate: `${year}-${month}-${day}`,
         isFetching: false,
         averages: Object.assign({}, state.averages, {
           [`${year}-${month}-${day}`]: action.averages
