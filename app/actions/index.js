62d44c3106d77c5bd94c0745abbf353b8b486c3e
Thr1ve <fict1on.buhler@gmail.com>
2016-03-21 03:06:55 -0400

    (feat) add averages actions

--- app/actions/index.js
+++ app/actions/index.js
@@ -38,3 +38,21 @@ export const searchKeyword = (input) =>
   dispatch => queries.getSearchResults(input)
     .then(messagesReceived => dispatch(receiveMessages(messagesReceived)))
     .catch(err => console.log(err));
+
+
+export const REQUEST_AVERAGES = 'REQUEST_AVERAGES';
+export const requestAverages = delimiter => (
+  { type: REQUEST_AVERAGES, delimiter }
+);
+
+export const RECEIVE_AVERAGES = 'RECEIVE_AVERAGES';
+export const receiveAverages = (averages, delimiter, date) => (
+  { type: RECEIVE_AVERAGES, averages, delimiter, date }
+);
+
+export const getAveragesByHour = date => dispatch => {
+  dispatch(requestAverages('BY_HOUR'));
+  return queries.getAvgMessagesByHour(date)
+    .then(averages => dispatch(receiveAverages(averages, 'BY_HOUR', date)))
+    .catch(err => console.log(err));
+};
