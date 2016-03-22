025e532af5e58e2d3ed718b8bf6b7ed8878a2ae2
steventsao <steventsao713@gmail.com>
2016-03-21 17:43:28 -0700

    Enable search on enter

--- app/components/SearchBox.js
+++ app/components/SearchBox.js
@@ -2,21 +2,27 @@ import React from 'react';
 
 
 const SearchBox = React.createClass({
-  onEnter(e){
+  handleKeyPress(e) {
     if (e.which === 13) {
-      this.props.handleKeyPress(this._input.value)
+      this.props.handleKeyPress(this._input.value);
     }
   },
-  render(){
+  handleChange() {
+    if (!this._input.value.length) this.props.disableFilter();
+  },
+  render() {
     return (
-      <input ref={ (ref) => this._input = ref }
-      // onChange={this.props.detectChange} 
-      onKeyPress={this.onEnter} 
-      className="input is-text-centered" 
-      type="text" 
-      placeholder="Ask Dream Bot Anything" />
+      <input ref= { (c) =>{ this._input = c; } }
+        type="text"
+        // onChange={this.props.detectChange}
+        onChange={this.handleChange}
+        onKeyPress={this.handleKeyPress}
+        // onKeyDown={this.onEnter}
+        className="input is-text-centered"
+        placeholder="Ask Dream Bot Anything"
+      />
       );
   }
-})
+});
 
 export default SearchBox;
