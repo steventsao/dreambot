82aa8b324cd6d14f4ce358f447f70b8fd609d7ef
Thr1ve <fict1on.buhler@gmail.com>
2016-03-22 01:13:20 -0400

    (refactor) reorganization

--- app/actions/averagesActions.js
+++ app/actions/averagesActions.js
@@ -1,19 +1,14 @@
 import { getAvgMessagesByHour } from './queries';
 
 export const REQUEST_AVERAGES = 'REQUEST_AVERAGES';
-export const requestAverages = delimiter => (
-  { type: REQUEST_AVERAGES, delimiter }
-);
-
 export const RECEIVE_AVERAGES = 'RECEIVE_AVERAGES';
+export const CHANGE_DATE = 'CHANGE_DATE';
+
+export const requestAverages = delimiter => ({ type: REQUEST_AVERAGES, delimiter });
 export const receiveAverages = (data, delimiter, date) => (
   { type: RECEIVE_AVERAGES, data, delimiter, date, receivedAt: Date.now() }
 );
-
-export const CHANGE_DATE = 'CHANGE_DATE';
-export const changeDate = (delimiter, date) => (
-  { type: CHANGE_DATE, delimiter, date }
-);
+export const changeDate = (delimiter, date) => ({ type: CHANGE_DATE, delimiter, date });
 
 export const getHours = date => dispatch => {
   dispatch(requestAverages('BY_HOUR'));
@@ -21,6 +16,14 @@ export const getHours = date => dispatch => {
     .then(data => dispatch(receiveAverages(data, 'BY_HOUR', date)))
     .catch(err => console.log(err));
 };
+
+export const getHoursIfNeeded = date =>
+  (dispatch, getState) => {
+    if (shouldFetchHours(getState(), date)) {
+      return dispatch(getHours(date));
+    }
+    return dispatch(changeDate('BY_HOUR', date));
+  };
 
 function shouldFetchHours(state, { year, month, day }) {
   const { available } = state.averages.byHour;
@@ -30,10 +33,3 @@ function shouldFetchHours(state, { year, month, day }) {
   return false;
 }
 
-export const getHoursIfNeeded = date =>
-  (dispatch, getState) => {
-    if (shouldFetchHours(getState(), date)) {
-      return dispatch(getHours(date));
-    }
-    return dispatch(changeDate('BY_HOUR', date));
-  };
