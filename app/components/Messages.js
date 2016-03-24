20508399ec8a18255d2368d5898457ed5ba3d576
danmolitor <danmolitor91@gmail.com>
2016-03-24 11:19:41 -0700

    (style) messages scrollable

--- app/components/Messages.js
+++ app/components/Messages.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import moment from 'moment';
+import styles from '../styles';
 
 const Messages = ({ messages, filterUserMessages }) => {
   const comparator = (a, b) => { return new Date(b.ts) - new Date(a.ts) };
   console.log()
   const sorted = messages.sort(comparator);
   return (
-    <div>
+    <div style={styles.ofprot}>
       {sorted.map(message => {
         let time = moment(new Date(message.ts));
         return (
