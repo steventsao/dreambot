442bedebea7ea0f3267ade250cf92683c8d61684
Thr1ve <fict1on.buhler@gmail.com>
2016-03-23 22:42:59 -0400

    (feat) remove averages from TestContainer

--- app/containers/TestContainer.js
+++ app/containers/TestContainer.js
@@ -1,30 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import moment from 'moment';
 
-import Graph from '../components/Graph';
+import { notify } from '../actions';
 import Test from '../components/Test';
 
-import { getHoursIfNeeded, notify } from '../actions';
-
 const TestContainer = React.createClass({
-  componentDidMount() {
-    // Grab today's data by default
-    this.props.dispatch(getHoursIfNeeded({
-      year: moment().year(),
-      month: moment().month() + 1,
-      day: moment().date()
-    }));
-  },
-
-  getAverages(date) {
-    const { year, month, day } = date;
-    if (!year || !month || !day ) {
-      return this.props.dispatch(notify('You must enter a year, month, and day', 'error'));
-    }
-    this.props.dispatch(getHoursIfNeeded(date));
-  },
-
   act() {
     if (!this.n) {
       this.n = 1;
@@ -34,47 +14,17 @@ const TestContainer = React.createClass({
   },
 
   render() {
-    const { labels, data } = this.props;
     return (
       <div>
         <Test {...this.props} />
         <button onClick={this.act}>act</button>
-        <Selector onSubmit={this.getAverages} />
-        <Graph labels={labels} data={data} />
       </div>
     );
   }
 });
 
 function mapStateToProps(state) {
-  const { available } = state.averages.byHour
-  const { year, month, day } = state.averages.byHour.displayedDate;
-  const key = `${year}-${month}-${day}`;
-  return {
-    labels: available[key] && available[key].data.map(obj => moment().hour(obj.group).format('hA')),
-    data: available[key] && available[key].data.map(obj => obj.reduction),
-  };
+  return state;
 }
 
 export default connect(mapStateToProps)(TestContainer);
-
-const Selector = React.createClass({
-  handleClick() {
-    this.props.onSubmit({
-      year: Number(this._year.value),
-      month: Number(this._month.value),
-      day: Number(this._day.value)
-    });
-  },
-
-  render() {
-    return (
-      <div>
-        <input type="input" placeholder="year" defaultValue="2016" ref={ref => this._year = ref} />
-        <input type="input" placeholder="month" ref={ref => this._month = ref} />
-        <input type="input" placeholder="day" ref={ref => this._day = ref} />
-        <button type="submit" onClick={this.handleClick}>submit</button>
-      </div>
-    );
-  }
-});
