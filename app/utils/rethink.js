a14a046f1f7577611739ba4d594ae94072aea79c
Thr1ve <fict1on.buhler@gmail.com>
2016-03-25 19:40:23 -0400

    (chore) get env variables from window

--- app/utils/rethink.js
+++ app/utils/rethink.js
@@ -3,8 +3,8 @@ import RethinkdbWebsocketClient from 'rethinkdb-websocket-client';
 // https://github.com/mikemintz/rethinkdb-websocket-client/tree/master/examples/tutorial
 // Open a WebSocket connection to the server to send RethinkDB queries over
 const options = {
-  host: RETHINK_HOST, // hostname of the websocket server
-  port: PORT,        // port number of the websocket server
+  host: window.RETHINK_HOST, // hostname of the websocket server
+  port: window.PORT,        // port number of the websocket server
   path: '/db',       // HTTP path to websocket route
   secure: false,     // set true to use secure TLS websockets
   db: 'test',        // default database, passed to rethinkdb.connect
