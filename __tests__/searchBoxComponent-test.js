616992f4c0ef14b616d83afa340504e0275ad8da
steventsao <steventsao713@gmail.com>
2016-03-26 15:43:20 -0700

    Cleanup tests

--- __tests__/searchBoxComponent-test.js
+++ __tests__/searchBoxComponent-test.js
@@ -12,22 +12,9 @@ describe('Search Box', () => {
   let searchBoxInstance = <SearchBox />
   let testSearchBox = TestUtils.renderIntoDocument(searchBoxInstance);
 
-  it('should exist', () => {
-    // expect(TestUtils.isDOMComponent(SearchBox())).toBeTruthy();
-  });
-
   it('should have refs', () => {
     console.log(searchBoxInstance.props)
     expect(searchBoxInstance.props).toBeTruthy();
   })
-
-  // it('should default with an empty string', () => {
-  //   TestUtils.Simulate.keyDown(node, {key: "Enter", keyCode: 13, which: 13});
-  //   expect(testSearchBox)
-  // });
-
-  // it('should bind user input', () => {
-
-  // });
 
 })
