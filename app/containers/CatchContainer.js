0fedcc20c8a6b70b1f2d0a343958c89b58509907
Thr1ve <fict1on.buhler@gmail.com>
2016-03-27 14:34:03 -0400

    (feat) save token in localstorage

--- app/containers/CatchContainer.js
+++ app/containers/CatchContainer.js
@@ -3,6 +3,7 @@ import React from 'react';
 const CatchContainer = React.createClass({
   componentDidMount(){
     console.log('THE TOKEN IS: ', this.props.location.query.token);
+    window.localStorage.setItem('token', this.props.location.query.token)
   },
 
   render() {
