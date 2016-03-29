e389f29059802600f11ae9a92f5721bf6de5e052
Thr1ve <fict1on.buhler@gmail.com>
2016-03-28 20:07:57 -0400

    (refactor) refactor envDefaults

--- server/utils/envDefaults.js
+++ server/utils/envDefaults.js
@@ -1,13 +1,33 @@
-export default {
-  port: process.env.PORT || 1337,
-  isDev: process.env.NODE_ENV !== 'production',
+export const port = process.env.PORT || 1337;
+export const isDev = process.env.NODE_ENV !== 'production';
 
-  // RethinkDb
-  rethinkHost: process.env.RETHINK_HOST || 'localhost',
-  rethinkPort: process.env.RETHINK_PORT || 28015,
-  rethinkDb: process.env.RETHINK_DB || 'test',
+// RethinkDb
+export const rethinkHost = process.env.RETHINK_HOST || 'localhost';
+export const rethinkPort = process.env.RETHINK_PORT || 28015;
+export const rethinkDb = process.env.RETHINK_DB || 'test';
 
-  // Auth
-  secret: process.env.SECRET || 'KEYBOARDLKJR;B34BQP3O4BGAKCATCATCAT434490',
-  allowedOrg: process.env.ALLOWED_ORG || 'hrr13-thedreamteam'
-};
+// Auth
+export const jwtSecret = process.env.SECRET || 'KEYBOARDLKJR;B34BQP3O4BGAKCATCATCAT434490';
+
+// Github Auth
+export const allowedOrg = process.env.ALLOWED_ORG || 'hrr13-thedreamteam';
+export const githubClientSecret = process.env.GITHUB_CLIENT_ID;
+export const githubClientId = process.env.GITHUB_CLIENT_SECRET;
+
+// Slack
+export const slackToken = process.env.token;
+
+if (!isDev && !process.env.SECRET) {
+  console.log('Error: You must specify a JWT Secret in production');
+  process.exit(1);
+}
+
+if (!githubClientId || !githubClientSecret) {
+  console.log('Error: Please specify a valid Github Client ID and Secret in environment');
+  process.exit(1);
+}
+
+if (!slackToken) {
+  console.log('Error: Specify slack token in environment');
+  process.exit(1);
+}
