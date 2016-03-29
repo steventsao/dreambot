e4009668733b8013ec926a05a404dc834f8bc8bc
danmolitor <danmolitor91@gmail.com>
2016-03-29 14:36:17 -0700

    (styles) updated graphs colors to hack-reactor blue, and changed style file to es6

--- app/components/Graph.js
+++ app/components/Graph.js
@@ -23,14 +23,14 @@ const Graph = ({labels = [], data = [], dataAvg = [], barChartDatasets = [], bar
 
           label: "Average Mood",
           fill: true,
-          backgroundColor: "rgba(220,220,220,0.2)",
-          borderColor: "rgba(220,220,220,1)",
-          pointBorderColor: "rgba(220,220,220,1)",
+          backgroundColor: "rgba(2,152,245,0.2)",
+          borderColor: "rgba(2,152,245,1)",
+          pointBorderColor: "rgba(2,152,245,1)",
           pointBackgroundColor: "#fff",
           pointBorderWidth: 3,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(220,220,220,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBackgroundColor: "rgba(2,152,245,1)",
+          pointHoverBorderColor: "rgba(2,152,245,1)",
           pointHoverBorderWidth: 2,
           data: data
         },
@@ -41,10 +41,11 @@ const Graph = ({labels = [], data = [], dataAvg = [], barChartDatasets = [], bar
     datasets: [
         {
             label: "Channel Engagement",
-            fillColor: "rgba(220,220,220,0.5)",
+            backgroundColor: "rgba(2,152,245,1)",
+            fillColor: "rgba(0220,220,220,0.5)",
             strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
+            highlightFill: "rgba(0220,220,220,0.75)",
+            highlightStroke: "rgba(0220,220,220,1)",
             data: barChartDatasets
         }
     ]
