9030bfb491bcac998f0a232c18a65df48655a70d
steventsao <steventsao713@gmail.com>
2016-03-30 10:33:29 -0700

    Remove browser listener

--- app/index.js
+++ app/index.js
@@ -29,9 +29,6 @@ let render = () => {
   );
 };
 
-
-browserHistory.listen(location => store.dispatch(routeLocationDidUpdate(location)));
-
 if (module.hot) {
   // Support hot reloading of components
   // and display an overlay for runtime errors
