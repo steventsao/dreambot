43c27f8f5b2d08a7bab078f69c5a6e61251c79cb
steventsao <steventsao713@gmail.com>
2016-03-30 01:31:37 -0700

    Add logic to sorting

--- app/components/Cohort.js
+++ app/components/Cohort.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router';
-export default function Cohort({members, profiles}){
+export default function Cohort({profiles, members}){
+  console.log(members, profiles, ' cohort')
   return (
     <div className="columns is-multiline">
-      {profiles.map(student => {
-        return members.indexOf(student.id) === -1
-        ? null
-        : (
-          <div key={student.id} className="card">
+      {members.map(student => {
+        if (profiles[student]) {
+        return (
+          <div className="column is-quarter">
+          <div key={profiles[student].id} className="card">
             <div className="card-image">
               <figure className="image is-4by3">
-                <img src={student.profile.image_192} alt="" />
+                <img src={profiles[student].profile.image_192} alt="" />
               </figure>
             </div>
             <div className="card-content">
               <div className="media">
                 <div className="media-content">
-                  <p className="title is-5">{student.name}</p>
-                   <Link className="subtitle is-6" to={`/user/${student.id}`}> Details </Link>
-                  <p className="subtitle is-6">{student.profile.email}</p>
+                  <p className="title is-5">{profiles[student].name}</p>
+                  <p className="title is-5">{profiles[student].wordCount}</p>
+                   <Link className="subtitle is-6" to={`/user/${profiles[student].id}`}> Details </Link>
+                  <p className="subtitle is-6">{profiles[student].profile.email}</p>
                 </div>
               </div>
 
               <div className="content">
-                <small>{student.tz}</small>
+                <small>{profiles[student].tz}</small>
               </div>
             </div>
           </div>
+          </div>
         )
+      }
       })}
     </div>
     )
