349a7a80ec2e3998cd6eb6d225ae1276fd2477df
Thr1ve <fict1on.buhler@gmail.com>
2016-03-30 02:26:12 -0400

    (feat) add react-router-redux and move configureStore to config folder

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,15 +1,17 @@
 import { combineReducers } from 'redux';
+import { routerReducer as routing } from 'react-router-redux';
 import messages from './messages';
 import searchBox from './SearchBoxReducer';
 import averages from './averagesReducer';
 import notifications from './notificationsReducer';
 import wordCount from './wordCountReducer';
-import classify from './questionReducer'
+import classify from './questionReducer';
 import engagement from './engagement';
 import profile from './userProfileReducer';
 import cohortProfiles from './cohortProfilesReducer';
 
 const rootReducer = combineReducers({
+  routing,
   messages,
   searchBox,
   averages,
