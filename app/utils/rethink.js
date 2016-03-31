3557a68b925f95fe73769c514ba8339e66f6fb8f
Thr1ve <fict1on.buhler@gmail.com>
2016-03-31 12:40:39 -0400

    (refactor) make rethink connection a function

--- app/utils/rethink.js
+++ app/utils/rethink.js
@@ -4,17 +4,19 @@
 
 import RethinkdbWebsocketClient from 'rethinkdb-websocket-client';
 
-const token = window.localStorage.getItem('token');
-const path = `/db?token=${encodeURIComponent(token)}`;
 
-// Open a WebSocket connection to the server to send RethinkDB queries over
-const options = {
-  host: HOST, // hostname of the websocket server
-  port: PORT, // port number of the websocket server
-  path: path,       // HTTP path to websocket route
-  secure: false,     // set true to use secure TLS websockets
-  db: 'test',        // default database, passed to rethinkdb.connect
+export const connection = function () {
+  const token = window.localStorage.getItem('token');
+  const path = `/db?token=${encodeURIComponent(token)}`;
+
+  // Open a WebSocket connection to the server to send RethinkDB queries over
+  const options = {
+    host: HOST, // hostname of the websocket server
+    port: PORT, // port number of the websocket server
+    path: path,       // HTTP path to websocket route
+    secure: false,     // set true to use secure TLS websockets
+    db: 'test',        // default database, passed to rethinkdb.connect
+  };
+  return RethinkdbWebsocketClient.connect(options);
 };
-
-export const connection = RethinkdbWebsocketClient.connect(options);
 export const r = RethinkdbWebsocketClient.rethinkdb;
